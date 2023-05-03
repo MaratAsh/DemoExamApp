ef4c3f0cb844d786907f1f1536a5d2860844c877
Marat Ashrafzianov <marat.ashraf@yandex.ru>
2023-05-04 00:16:51 +0300

    Product Window

--- Windows/ProductsWindow.xaml.cs
+++ Windows/ProductsWindow.xaml.cs
@@ -22,6 +22,8 @@ namespace WpfApp.Windows
     {
         Models.User? user;
         Context context;
+        public List<string> filterSales { get; set; }
+        public List<string> sortTypes { get; set; }
         public List<Models.Product> products { get; set; }
         public ProductsWindow(Models.User? user)
         {
@@ -31,6 +33,70 @@ namespace WpfApp.Windows
             context = new Context();
             context.Products.Load();
             products = context.Products.ToList();
+            filterSales = new List<string>();
+            filterSales.Add("Все диапазоны");
+            filterSales.Add("0-9,99%");
+            filterSales.Add("10-14,99%");
+            filterSales.Add("15% и более");
+            sortTypes = new List<string>();
+            sortTypes.Add("Без сортировки");
+            sortTypes.Add("Сортировка по возрастанию");
+            sortTypes.Add("Сортировка по убыванию");
+            productCalculation();
+        }
+
+        private void filterListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            productCalculation();
+        }
+
+        private void sortListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            productCalculation();
+        }
+
+        private void searchQueryBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            productCalculation();
+        }
+
+        private void searchQueryButton_Click(object sender, RoutedEventArgs e)
+        {
+            searchQueryBox.Text = "";
+            productCalculation();
+        }
+
+        private void productCalculation()
+        {
+            IQueryable<Models.Product> p = context.Products;
+            if (searchQueryBox.Text != "")
+            {
+                p = p.Where(prod => prod.ProductName.StartsWith(searchQueryBox.Text)
+                                    || prod.ProductName.EndsWith(searchQueryBox.Text));
+            }
+
+            if (sortListBox.SelectedItem as string == "Сортировка по возрастанию")
+            {
+                p = p.OrderBy(prod => prod.ProductMaxDiscountAmount);
+            }
+            else if (sortListBox.SelectedItem as string == "Сортировка по убыванию")
+            {
+                p = p.OrderByDescending(prod => prod.ProductMaxDiscountAmount);
+            }
+
+            if (filterListBox.SelectedItem as string == "0-9,99%")
+            {
+                p = p.Where(prod => prod.ProductMaxDiscountAmount < 10);
+            }
+            else if (filterListBox.SelectedItem as string == "10-14,99%")
+            {
+                p = p.Where(prod => prod.ProductMaxDiscountAmount < 15);
+            }
+            else if (filterListBox.SelectedItem as string == "15% и более")
+            {
+                p = p.Where(prod => prod.ProductMaxDiscountAmount >= 15);
+            }
+            products = p.ToList();
         }
     }
 }
